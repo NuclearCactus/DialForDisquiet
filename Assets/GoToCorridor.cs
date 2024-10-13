1bb98904d07b26d7e7518167609b3e67a75f5bac
Soham Sen <soham2014sen@gmail.com>
2024-10-13 12:34:35 +0100

    Fixed InitialScene and FinalScene

--- Assets/GoToCorridor.cs
+++ Assets/GoToCorridor.cs
@@ -5,8 +5,9 @@ public class GoToCorridor : MonoBehaviour
     public PlayerMovement1 player;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject == player)
+        if (collision.gameObject == player.gameObject)
         {
+            Debug.Log("Entering");
             SceneManager.LoadScene("CorridorScene");
         }
     }
